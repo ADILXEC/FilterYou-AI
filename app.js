06f6c6c0b3c1c880efa2236253373c9e95aa0d35
lsg360 <154537763+lsg360@users.noreply.github.com>
2024-03-26 17:34:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ const getImages = async () => {
         },
         body: JSON.stringify({
             "engine_id": "stable-diffusion-xl-1024-v1-0",
-            "height": 768,
-            "width": 1344,
+            "height": 1024,
+            "width": 1024,
             "text_prompts": [
                 {
                     "text": promptText,
