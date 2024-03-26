6ab5a2c867b23fc4c10555af8fed92723320f1f1
lsg360 <154537763+lsg360@users.noreply.github.com>
2024-03-26 16:12:02 +0530

    Added 3 files
    has code of api reference from stability ai
    i used javascript html css

---
+++ app.js
@@ -0,0 +1,74 @@
+const API_KEY = "sk-26bcY9xYmBCgN8KyIOov6faWV68nyIVOHJuCxh7A55wyXYqG";
+
+const submitIcon = document.querySelector("#submit-icon");
+const imagesSection = document.querySelector(".images-section");
+
+const inputElement = document.querySelector("#inputPrompt");
+
+const getImages = async () => {
+    const promptText = inputElement.value.trim();
+
+    if (!promptText) {
+        console.error('Please enter some text prompts.');
+        return;
+    }
+
+    console.log(JSON.stringify({
+        "text_prompts": promptText,
+        "n": 4,
+        "size": "512x512"
+    }));
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "engine_id": "stable-diffusion-xl-1024-v1-0",
+            "height": 640,
+            "width": 1536,
+            "text_prompts": [
+                {
+                    "text": promptText,
+                    "weight": 0.5
+                }
+            ],
+            "cfg_scale": 7,
+            "clip_guidance_preset": "NONE",
+            "samples": 1,
+            "seed": 0,
+            "steps": 30,
+            "style_preset": "photographic",
+        })
+    };
+
+    try {
+        const response = await fetch('https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/text-to-image', options);
+        const data = await response.json();
+
+        console.log(data); // Log the response data
+
+        if (response.ok) {
+            console.log(data);
+            if (data.artifacts && data.artifacts.length > 0) {
+                imagesSection.innerHTML = '';
+                data.artifacts.forEach(artifact => {
+                    const img = document.createElement('img');
+                    img.src = `data:image/png;base64,${artifact.base64}`;
+                    imagesSection.appendChild(img);
+                });
+            } else {
+                console.log('No images found in the API response');
+            }
+        } else {
+            console.error(`Error: ${data.name} - ${data.message}`);
+            console.error(`ID: ${data.id}`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+submitIcon.addEventListener('click', getImages);
